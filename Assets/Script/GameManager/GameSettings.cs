185d5775eff369f619c1c57582b8c9d67f4bbf65
Ngia.D.PC <diephieunghia@ymail.com>
2025-08-18 21:38:47 +0700

    Pause WIP

--- Assets/Script/GameManager/GameSettings.cs
+++ Assets/Script/GameManager/GameSettings.cs
@@ -12,12 +12,27 @@ public class GameSettings: MonoBehaviour
     public float mouseSensivity = 100f;
     public float brightness = 1f;
 
+    //Pause Menu
+    public GameObject pauseMenu;
+
     private void Awake()
     {
         if (ins != null && ins != this)
             Destroy(this);
         else
             ins = this;
+    }
+    void Update()
+    {
+        SetGamePause();
+    }
+    void SetGamePause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+            Time.timeScale = isGamePaused ? 0f : 1f;
+        }
     }
 
 }
